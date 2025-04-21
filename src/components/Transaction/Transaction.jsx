1041b105d7e583a83b578f64bb36fc6fb08ac0b7
Vladissluff <165704824+Vladissluff@users.noreply.github.com>
2025-04-21 20:56:39 +0300

    react

---
+++ src/components/Transaction/Transaction.jsx
@@ -0,0 +1,26 @@
+import styles from "./Transaction.module.css";
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={styles.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }, index) => (
+          <tr key={id} className={index % 2 === 0 ? styles.whiteRow : styles.grayRow}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
